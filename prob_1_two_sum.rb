6ba57fce941d16b2b25b679aff985d047973e653
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-19 21:26:48 -0700

    Added Solution 1 - 0(n^2) running time and 0(1) space

--- prob_1_two_sum.rb
+++ prob_1_two_sum.rb
@@ -6,3 +6,25 @@
 
 # Input: numbers={2, 7, 11, 15}, target=9
 # Output: index1=1, index2=2
+
+# SOLUTION 1: 0(n^2) running time and 0(1) space
+def two_sum(nums, target)
+  len = nums.length
+  for i in (0...len)
+    element = nums[i]
+    for j in (i+1...len)
+      if element + nums[j] == target
+        puts "index1=#{i+1}, index2=#{j+1}"
+        return
+      end
+    end
+  end
+
+end
+
+# TEST DRIVE
+
+puts "Testing Solution 1 - 0(n^2) running time and 0(1) space:"
+two_sum([2, 7, 11, 15], 9)
+two_sum([1, 4, 45, 6, 10, 8], 16)
+puts "\n"
