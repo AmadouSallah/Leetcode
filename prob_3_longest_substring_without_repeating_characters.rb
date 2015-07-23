a9e73467fb74fd990abfcfd6f13e21cb4c8225fd
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-22 18:19:41 -0700

    Added solution to prob_3_longest_substring_without_repeating_characters.rb

--- prob_3_longest_substring_without_repeating_characters.rb
+++ prob_3_longest_substring_without_repeating_characters.rb
@@ -1,2 +1,30 @@
 # Given a string, find the length of the longest substring without repeating characters.
 # For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+
+def longest_substring_without_repeating_characters(string)
+  len = string.length
+  return len if len < 2
+
+  i, j, max = 0, 0, 0
+  hash = {}
+  while i < len && j < len
+    char = string[j]
+    if !hash[char] # the current character is non repeating
+      hash[char] = 1
+      diff = j-i+1
+      max = (max > diff) ? max : diff
+      j += 1
+    else # the current character is repeating
+      diff = j-i
+      max = (max > diff) ? max : diff
+      i += 1
+    end
+  end
+  max
+end
+
+p longest_substring_without_repeating_characters("abcabcbb") == 3
+p longest_substring_without_repeating_characters("bbbbb") == 1
+p longest_substring_without_repeating_characters("abc") == 3
+p longest_substring_without_repeating_characters("a") == 1
+p longest_substring_without_repeating_characters("") == 0
